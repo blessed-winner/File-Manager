13cc5da07b68cf9df69103b2163261aa8a51c72e
blessed-winner <blessedwinner66@gmail.com>
2026-01-13 21:50:30 +0200

    Cloudinary provider added

---
+++ src/cloudinary/cloudinary.provider.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from '@nestjs/config'
+import { v2 as cloudinary } from 'cloudinary'
+
+
+export const CloudinaryProvider = {
+    provide: 'CLOUDINARY',
+    useFactory: (configService: ConfigService) => {
+        cloudinary.config({
+           cloud_name: configService.get<string>('CLD_CLOUD_NAME'),
+           api_key: configService.get<string>('CLD_API_KEY'),
+           api_secret: configService.get<string>('CLD_SECRET_KEY')
+        })
+
+        return cloudinary;
+    },
+
+    Inject: [ConfigService],
+}
