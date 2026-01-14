8b4cdddea5b2fed961245b10900bab7a2c4882a1
blessed-winner <blessedwinner66@gmail.com>
2026-01-14 09:20:45 +0200

    Database configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileModule } from './file/file.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService} from '@nestjs/config';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
 @Module({
   imports: [
@@ -11,6 +12,21 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal:true,
       envFilePath:'.env'
+    }),
+    CloudinaryModule,
+    TypeOrmModule.forRootAsync({
+       imports: [ ConfigModule ],
+       inject: [ ConfigService ],
+       useFactory:(config: ConfigService) => ({
+           type:'postgres',
+           database:config.get<string>('DB_NAME'),
+           host:config.get<string>('DB_HOST'),
+           port:Number(config.get<string>('DB_PORT')),
+           username:config.get<string>('DB_USERNAME'),
+           password:config.get<string>('DB_PASSWORD'),
+           entities: [ ],
+           synchronize:true
+       }) 
     })
   ],
   controllers: [AppController],
---
+++ src/cloudinary/cloudinary.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { CloudinaryProvider } from "./cloudinary.provider";
+
+@Module({
+    providers: [ CloudinaryProvider ],
+    exports: [ CloudinaryProvider ]
+})
+
+export class CloudinaryModule {}
