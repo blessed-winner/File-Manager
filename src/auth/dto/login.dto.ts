46ed2a3c2464dd995c50380833d77c5ffd7ce273
Winner <blessedwinner66@gmail.com>
2026-01-29 20:20:57 +0200

    Finished docker build for new changes

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,14 @@
-import { IsEmail, IsString } from "class-validator"
+import { IsEmail, IsOptional, IsString } from "class-validator"
 
 export class LoginDto{
     @IsString()
     @IsEmail()
+    @IsOptional()
     email:string
 
+
     @IsString()
+    @IsOptional()
     username:string
 
     @IsString()
