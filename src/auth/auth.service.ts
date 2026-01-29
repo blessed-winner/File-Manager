25384add45649c81097dca134918c58faddf7fac
Winner <blessedwinner66@gmail.com>
2026-01-29 14:37:07 +0200

    Defined user entity and authentication flow

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class AuthService{
+    constructor(
+        @InjectRepository(User) private userRepo: Repository<User>
+    ){}
+}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class User{
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+    @Column({unique:true})
+    username:string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName:string;
+
+    @Column({unique:true})
+    email:string;
+
+    @Column()
+    password:string;
+
+    @Column({ type: 'date', nullable: true })
+    dateOfBirth:Date;
+
+    @Column({default:false})
+    isVerified:boolean;
+}
