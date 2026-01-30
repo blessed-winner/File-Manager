69702e17794fe3bf928354b0b7f315d3bf1e8e7a
Winner <blessedwinner66@gmail.com>
2026-01-30 16:58:37 +0200

    Jwt token generation feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,34 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
 import { Repository } from "typeorm";
 import { LoginDto } from "./dto/login.dto";
 import { SignUpDto } from "./dto/signup.dto";
+import * as bcrypt from 'bcrypt';
+import { NotFoundError } from "rxjs";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class AuthService{
     constructor(
-        @InjectRepository(User) private userRepo: Repository<User>
+        @InjectRepository(User) private userRepo: Repository<User>,
+        private readonly jwt:JwtService,
+        private readonly config:ConfigService
     ){}
 
     async signUp(dto:SignUpDto){
-        
+        const hashedPassword = await bcrypt.hash(dto.password,10);
+        const newUser = this.userRepo.create({
+            ...dto,
+            password:hashedPassword
+        })
+
+        await this.userRepo.save(newUser)
+
+        return {
+            message:"User registered successfully"
+        }
     }
 
     private async findUser(email?:string, username?:string){
@@ -25,8 +41,20 @@ export class AuthService{
     }
 
 
-    private async generateToken(){
+    private async generateToken(email:string){
+         const user = await this.userRepo.findOne({where: { email}})
+         if(!user){
+            throw new NotFoundException("Cannot find user for token generation!")
+         }
 
+         const payload = { sub: user.id, email: user.email, username:user.username}
+
+         const access_token = await this.jwt.signAsync(payload, {
+            secret:this.config.get<string>('JWT_SECRET'),
+            expiresIn:'15m'
+         })
+
+         return access_token
     }
 
 
